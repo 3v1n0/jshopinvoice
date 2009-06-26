7b56e5a064940005223a5acf6a7f05008528cd1d
trevi55 <3v1n0@users.noreply.github.com>
2009-06-26 22:39:03 +0000

    Tv set, indentation fixes

--- MP/src/item/TVSet.java
+++ MP/src/item/TVSet.java
@@ -1,12 +1,12 @@
 package item;
 
-public class TVSet extends Item{
+public class TVSet extends Item {
 	
 	private Float inches;
 
-	public TVSet(String nm, String br, Float pr, String desc,Float inch){
-	super(nm,br,pr,desc);
-	inches =inch;
+	public TVSet(String nm, String br, Float pr, String desc, Float in) {
+		super(nm, br, pr, desc);
+		inches = in;
 	}
 
 	public Float getInches() {return inches;}
