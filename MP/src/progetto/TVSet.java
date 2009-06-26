6396d6ccf46d2afcbd411519fbb9afc6faa853a3
matteo.pratesi@libero.it <matteo.pratesi@libero.it@86afc3da-6248-11de-876f-d39545802920>
2009-06-26 12:59:34 +0000

    update TVSet

--- MP/src/progetto/TVSet.java
+++ MP/src/progetto/TVSet.java
@@ -7,6 +7,12 @@ public class TVSet implements Item{
 	Double price;
 	String description;
 
+	public TVSet(String nm, String br, Double pr, String desc){
+	name = nm;
+	brand = br;
+	price = pr;
+	description = desc;
+	}
 	public String getBrand() {return brand;}
 	public String getDescription() {return description;}
 	public String getName() {return name;}
