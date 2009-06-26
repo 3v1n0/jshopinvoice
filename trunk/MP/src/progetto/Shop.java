5ab9f4808748003890b3438f225329b1778755be
trevi55 <trevi55@86afc3da-6248-11de-876f-d39545802920>
2009-06-26 15:20:02 +0000

    Added shop...

---
+++ trunk/MP/src/progetto/Shop.java
@@ -0,0 +1,25 @@
+package progetto;
+
+import java.util.LinkedList;
+
+import people.Company;
+
+public class Shop {
+	private Company company;
+	private String name;
+	private LinkedList<Item> items;
+	private int invoicenum;
+	private static int id;
+	
+	Shop(Company c, String n) {
+		company = c;
+		name = n;
+		invoicenum = 0;
+		items = new LinkedList<Item>();
+		id++;
+	}
+	
+	public void addItem(Item i) {
+		items.add(i);
+	}
+}
