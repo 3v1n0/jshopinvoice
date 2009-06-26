9ad7b12e0d729bde554b0f26e2ecdf246a85f563
trevi55 <3v1n0@users.noreply.github.com>
2009-06-26 22:37:28 +0000

    Added channels support to speakers

--- MP/src/item/Speakers.java
+++ MP/src/item/Speakers.java
@@ -1,14 +1,17 @@
 package item;
 
-public class Speakers extends Item{
+public class Speakers extends Item {
 
 	private Integer power;
+	private Integer channels;
 	
-	Speakers(String nm, String br, Float pr, String desc,Integer pwr) {
+	Speakers(String nm, String br, Float pr, String desc, Integer pwr, Integer ch) {
 		super(nm, br, pr, desc);
 		power = pwr;
+		channels = ch;
 	}
 
 	public Integer getPower() {return power;}
+	public Integer getChannels() {return channels;}
 
 }
