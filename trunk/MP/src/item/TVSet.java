23f3b3076354d03117749936859f174e7d551690
trevi55 <trevi55@86afc3da-6248-11de-876f-d39545802920>
2009-06-26 22:43:39 +0000

    Indentation and signature changes

--- trunk/MP/src/item/TVSet.java
+++ trunk/MP/src/item/TVSet.java
@@ -5,7 +5,7 @@ public class TVSet extends Item {
 	private Float inches;
 
 	public TVSet(String nm, String br, Float pr, String desc, Float in) {
-		super(nm, br, pr, desc);
+		super(br, nm, desc, pr);
 		inches = in;
 	}
 
