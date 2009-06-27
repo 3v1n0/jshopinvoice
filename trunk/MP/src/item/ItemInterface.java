28ba688cb03b3c132133e80bd00079250f985aaf
trevi55 <trevi55@86afc3da-6248-11de-876f-d39545802920>
2009-06-27 03:21:16 +0000

    ItemInterface, add getInfo() (still commented).

--- trunk/MP/src/item/ItemInterface.java
+++ trunk/MP/src/item/ItemInterface.java
@@ -5,5 +5,6 @@ public interface ItemInterface {
 	public String getBrand();
 	public String getDescription();
 	public String getName();
+	//public String getInfo(); //TODO this should be class-specific.
 	public Float getPrice();
 }
