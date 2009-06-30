fb29eb27c92796b36faec4d1dc4531d5051fa701
trevi55 <trevi55@86afc3da-6248-11de-876f-d39545802920>
2009-06-30 02:18:04 +0000

    Invoice skeleton (TBD)

--- trunk/MP/src/progetto/Invoice.java
+++ trunk/MP/src/progetto/Invoice.java
@@ -1,5 +1,13 @@
 package progetto;
 
+import entities.*;
+import java.util.Date;
+
 public class Invoice {
+	private Shop seller;
+	private Entity buyer;
+	private Date time;
+	private int id;
+	//TODO keep trace of the duplicates
 
 }
