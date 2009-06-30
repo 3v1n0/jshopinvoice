fdbbf4c3fbddb99e885dac7bb2e8e11da294d52f
matteo.pratesi@libero.it <matteo.pratesi@libero.it@86afc3da-6248-11de-876f-d39545802920>
2009-06-30 18:03:53 +0000

    Goditi questa meravigliadi mix di Decorator e Factory.. ;)

---
+++ MP/src/item/Sconto.java
@@ -0,0 +1,23 @@
+package item;
+
+public abstract class Sconto extends Item{
+
+	Item obj;
+	Integer percent;
+	
+	protected Sconto(Item a){
+		obj = a;
+		percent = createSconto();		
+	}
+	protected abstract Integer createSconto();
+	
+	public String getBrand() {return obj.getBrand();}
+	public String getDescription() {return obj.getDescription();}
+	public String getName() {return obj.getName();}
+	public Float getPrice() {return (obj.getPrice()-(obj.getPrice()*percent/100));}
+	public int getId() {return obj.getId();}
+	
+	public void add(Item i)throws SinglePartException{	obj.add(i);	}
+	public void remove(Item i)throws SinglePartException{	obj.remove(i); }
+	public Item getChild(int n){ return obj.getChild(n);}
+}
---
+++ MP/src/item/Sconto5.java
@@ -0,0 +1,11 @@
+package item;
+
+public class Sconto5 extends Sconto{
+	
+	public Sconto5(Item obj){
+		super(obj);
+	}
+
+	protected Integer createSconto() {	return 5;	}
+
+}
