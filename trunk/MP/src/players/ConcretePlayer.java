cbdc7805bb995131d2b916f7bc44d048d01cf015
trevi55 <trevi55@86afc3da-6248-11de-876f-d39545802920>
2009-06-30 15:59:36 +0000

    A last fix to ConcretePlayer

--- trunk/MP/src/players/ConcretePlayer.java
+++ trunk/MP/src/players/ConcretePlayer.java
@@ -3,40 +3,30 @@ package players;
 import item.*;
 import media.Media;
 
-public abstract class ConcretePlayer extends Item implements Player{
-
+public abstract class ConcretePlayer extends Item implements Player {
 	private Media read;
+	@SuppressWarnings("unused") private ConcretePlayer pl;
 
-	private ConcretePlayer pl;
-	
-	ConcretePlayer(){}
-	ConcretePlayer(String name,String brand,Float price,String description){
+	ConcretePlayer() {}
+	ConcretePlayer(String name,String brand,Float price,String description) {
 		super (brand, name, description, price);
-		pl=createPlayer();
+		pl = createPlayer();
 	}
 	
 	abstract ConcretePlayer createPlayer();
 	
-	public void accept(Media m){
+	public void accept(Media m) {
 		read = m;
 	}
 	
 	public String play() {
 		String s;
+		
 		if( read == null )
-		s = "Error: Insert a media.";
+			s = "Error: Insert a media.";
 		else
-		s = "Name: "+this.getName()+" Brand: "+this.getBrand()+" read \n\n"+read.getInfo();
+			s = "Name: "+this.getName()+" Brand: "+this.getBrand()+" read \n\n"+read.getInfo();
+
 		return s;
-		}
-	
-	public void add(Item i)throws SinglePartException{
-		throw new SinglePartException();		
-		}
-		
-		public void remove(Item i)throws SinglePartException{
-		throw new SinglePartException();		
-		}
-		
-		public Item getChild(int n){ return null;}
+	}
 }
