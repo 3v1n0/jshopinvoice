9c58cdfdb3b57164beace2eaa59081deba4d476d
trevi55 <3v1n0@users.noreply.github.com>
2009-06-30 16:01:25 +0000

    Let the DVDPlayer read also the CDs

--- MP/src/players/DVDPlayer.java
+++ MP/src/players/DVDPlayer.java
@@ -2,17 +2,17 @@ package players;
 
 import media.*;
 
-public class DVDPlayer extends ConcretePlayer{
+public class DVDPlayer extends ConcretePlayer {
 
-	private DVDPlayer(){}
-	public DVDPlayer(String title,String brand,String description, Float price){
+	private DVDPlayer() {}
+	public DVDPlayer(String title,String brand,String description, Float price) {
 		super(title,brand,price,description);
 	}
 	
-	ConcretePlayer createPlayer(){return new DVDPlayer();}
+	ConcretePlayer createPlayer() {return new DVDPlayer();}
 
 	public void accept(Media m){
-		if(m instanceof DVD || m instanceof DVDAudio) {super.accept(m);}
+		if (m instanceof DVD || m instanceof DVDAudio || m instanceof CD) super.accept(m);
 		else System.out.println("Media non corretto per il tipo di lettore.");
 	}
 }
