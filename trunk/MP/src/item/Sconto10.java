37a423aff5b4362a341a9fe2ccbf54835a092171
matteo.pratesi@libero.it <matteo.pratesi@libero.it@86afc3da-6248-11de-876f-d39545802920>
2009-06-30 18:03:53 +0000

    Goditi questa meravigliadi mix di Decorator e Factory.. ;)

---
+++ trunk/MP/src/item/Sconto10.java
@@ -0,0 +1,11 @@
+package item;
+
+public class Sconto10 extends Sconto{
+
+	public Sconto10(Item obj){
+		super(obj);
+	}
+
+	protected Integer createSconto() {	return 10;	}
+
+}
---
+++ trunk/MP/src/item/Sconto20.java
@@ -0,0 +1,11 @@
+package item;
+
+public class Sconto20 extends Sconto{
+	
+	public Sconto20(Item obj){
+		super(obj);
+	}
+
+	protected Integer createSconto() {	return 20;	}
+
+}
