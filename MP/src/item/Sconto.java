aae7ff8c23d90af65009e5a22329891571ca216c
matteo.pratesi@libero.it <matteo.pratesi@libero.it@86afc3da-6248-11de-876f-d39545802920>
2009-07-01 10:49:34 +0000

    guarda il commento

--- MP/src/item/Sconto.java
+++ MP/src/item/Sconto.java
@@ -11,13 +11,20 @@ public abstract class Sconto extends Item{
 	}
 	protected abstract Integer createSconto();
 	
-//	public String getBrand() {return obj.getBrand();}
-//	public String getDescription() {return obj.getDescription();}
-//	public String getName() {return obj.getName();}
-	public Float getPrice() {return (obj.getPrice()-(obj.getPrice()*percent/100));}
-//	public int getId() {return obj.getId();}
+/*
+ *  Il decorator avvolge l'oggetto con un nuovo oggetto che per come lo creiamo noi ha i campi nome brand ecc
+ *  nulli. se non facciamo l'overload dei metodi questi ritornano valori nulli mentre noi vogliamo che 
+ *  ritornino i valori dell'oggetto avvolto, e questo lo otteniamo con la delegation a obj. E' descritto
+ *  anche nel libro dei pattern. :p
+ */ 
 	
-//	public void add(Item i)throws SinglePartException{	obj.add(i);	}
-//	public void remove(Item i)throws SinglePartException{	obj.remove(i); }
-//	public Item getChild(int n){ return obj.getChild(n);}
+	public String getBrand() {return obj.getBrand();}
+	public String getDescription() {return obj.getDescription();}
+	public String getName() {return obj.getName();}
+	public Float getPrice() {return (obj.getPrice()-(obj.getPrice()*percent/100));}
+	public int getId() {return obj.getId();}
+	
+	public void add(Item i)throws SinglePartException{	obj.add(i);	}
+	public void remove(Item i)throws SinglePartException{	obj.remove(i); }
+	public Item getChild(int n){ return obj.getChild(n);}
 }
