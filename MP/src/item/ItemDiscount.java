9ea95917c1c1e2839cc107aeb13e071112a25ca8
trevi55 <3v1n0@users.noreply.github.com>
2009-07-08 15:08:41 +0000

    Argggg...! FIXME removal, again!

--- MP/src/item/ItemDiscount.java
+++ MP/src/item/ItemDiscount.java
@@ -44,11 +44,6 @@ public abstract class ItemDiscount extends Item {
 			return false;
 		
 		Item it = (Item)i;
-		/*
-		System.out.println(getPrice()+" = "+it.getPrice()+" "+(this.getDiscount().equals(it.getDiscount()))+" "+(this.getDiscount().equals(it.getDiscount()))+
-				" "+(item.equals(it) && this.getDiscount().equals(it.getDiscount()))+" "+item.equals(it));
-		*/
-//		return (item.equals(it) && this.getDiscount().equals(it.getDiscount())); //FIXME controllo sul prezzo
 		return item.equals(it);
 	}
 
